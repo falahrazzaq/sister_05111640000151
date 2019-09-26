b2cc6aa9a832f535ae9765e0ca14f28f3e11be73
falahrazzaq <32873491+falahrazzaq@users.noreply.github.com>
2019-09-26 12:09:24 +0000

    Update greet_client.py

--- tugas2/greet_client.py
+++ tugas2/greet_client.py
@@ -1,14 +1,33 @@
 import Pyro4
+import shlex
+import serpent
 
-def test_no_ns():
-    uri = "PYRO:obj_27d7c59497c44c688319f7d8a4a95935@localhost:40549"
-    gserver = Pyro4.Proxy(uri)
-    print(gserver.get_greet('ronaldo'))
+def send_with_ns(file_name):
+    uri = "PYRONAME:greetserver@localhost:7777"
+    with Pyro4.Proxy(uri) as gserver:
+        with open(file_name, "rb") as f:
+            byte = f.read(2048)
+            while byte != "":
+                ser = serpent.dumps(byte, indent=True)
+                ser.decode("UTF-8")
+                gserver.construct_file(file_name,ser)
+                byte = f.read(2048)
+            gserver.construct_file(file_name,"FileSent//LERUfic")
 
-def test_with_ns():
+def test_with_ns(command):
     uri = "PYRONAME:greetserver@localhost:7777"
     gserver = Pyro4.Proxy(uri)
-    print(gserver.get_greet('ronaldo'))
+    print(gserver.routing(command))
 
 if __name__=='__main__':
-    test_with_ns()
+    runner_flag = True
+    while runner_flag:
+        command = raw_input("root@server:~$ ")
+        if command == "exit":
+            print "Bye"
+            break
+        elif "SEND" in command:
+            s_command = shlex.split(command)
+            send_with_ns(s_command[1])
+        else:
+            test_with_ns(command)
