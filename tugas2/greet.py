e57862e2b7464b352352335bb2635f3a5026bfac
falahrazzaq <32873491+falahrazzaq@users.noreply.github.com>
2019-09-26 12:08:17 +0000

    Update greet.py

--- tugas2/greet.py
+++ tugas2/greet.py
@@ -1,15 +1,63 @@
 import random
-
+import subprocess
+import shlex
+import serpent
 
 class GreetServer(object):
     def __init__(self):
         pass
 
-    def get_greet(self, name='NoName'):
-        lucky_number = random.randint(1, 100000)
-        return "Hello {}, this is your lucky number {}".format(name, lucky_number)
+    def get_ls(self):
+        return subprocess.check_output(["ls","-a","-l"])
+
+    def delete_rm(self,file_name):
+        return subprocess.check_output(["rm", file_name])
+
+    def read_cat(self,file_name):
+        return subprocess.check_output(["cat", file_name])
+    
+    def touch_mytralala(self,file_name):
+        return subprocess.check_output(["touch", file_name])
+
+    def write_update(self,file_name,option,text):
+        if option == "append":
+            with open(file_name, "a") as myfile:
+                myfile.write(text+'\n')
+        return subprocess.check_output(["cat", file_name])
+
+    def routing(self,command):
+        _gs = GreetServer()
+        if "GET" in command:
+            return _gs.get_ls()
+        elif "DEL" in command:
+            s_command = shlex.split(command)
+            return _gs.delete_rm(s_command[1])
+        elif "Touch" in command:
+            s_command = shlex.split(command)
+            return _gs.touch_mytralala(s_command[1])
+        elif "READ" in command:
+            s_command = shlex.split(command)
+            return _gs.read_cat(s_command[1])
+        elif "WRITE" in command:
+            # WRITE haha.txt append "text"
+            s_command = shlex.split(command)
+            return _gs.write_update(s_command[1], s_command[2], s_command[3])
+        else:
+            return "{}: command not found!".format(command)
+
+    def construct_file(self,file_name,data):
+        data = str(data)
+        f = open(file_name+'lerufic','ab')
+        if data == "FileSent//LERUfic":
+            f.close()
+            return subprocess.check_output(["mv", file_name+'lerufic',file_name])
+        else:
+            f = open(file_name+'lerufic','ab')
+            ser = serpent.loads(data)
+            f.write(ser.encode('utf-8'))
+            f.close()
+            return
 
 
 if __name__ == '__main__':
     k = GreetServer()
-    print(k.get_greet('royyana'))
