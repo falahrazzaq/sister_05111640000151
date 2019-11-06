2793a6f8271f09fe806a67d47460a62e79226f13
falahrazzaq <32873491+falahrazzaq@users.noreply.github.com>
2019-11-06 20:07:57 +0700

    finish

--- servers/c1/server.py
+++ servers/c1/server.py
@@ -1,16 +1,10 @@
-from fileserver import  *
-import Pyro4
 import sys
+sys.path.append("..")
 
-namainstance = "fileserver"
+from clients.backend import *
+import Pyro4
 
-def start_without_ns():
-    daemon = Pyro4.Daemon()
-    x_FileServer = Pyro4.expose(FileServer)
-    uri = daemon.register(x_FileServer)
-    print("my URI : ", uri)
-    daemon.requestLoop()
-
+namainstance = "fileserver1"
 
 def start_with_ns():
     #name server harus di start dulu dengan  pyro4-ns -n localhost -p 7777
@@ -19,12 +13,11 @@ def start_with_ns():
 
     daemon = Pyro4.Daemon(host="localhost")
     ns = Pyro4.locateNS("localhost",7777)
-    x_FileServer = Pyro4.expose(FileServer)
+    x_FileServer = Pyro4.expose(Backend)
     uri_fileserver = daemon.register(x_FileServer)
-    ns.register("{}" . format(namainstance), uri_fileserver)
+    #ns.register("{}" . format(namainstance), uri_fileserver)
     #untuk instance yang berbeda, namailah fileserver dengan angka
-    #ns.register("fileserver2", uri_fileserver)
-    #ns.register("fileserver3", uri_fileserver)
+    ns.register(namainstance, uri_fileserver)
     daemon.requestLoop()
 
 
